a73be5cccca3630ec8646c5954f81e192d755865
Heizenberg <popov.roman91@gmail.com>
2023-10-13 10:23:35 +0300

    added open function and chaged functionality to run function

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use clap::{Arg, App};
 use std::error::Error;
-
+use std::io::{self, BufRead, BufReader};
+use std::fs::File;
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 #[derive(Debug)]
@@ -13,7 +14,12 @@ pub struct Config {
 }
 
 pub fn run(config: Config) -> MyResult<()> {
-    println!("{:#?}", config);
+    for filename in &config.files {
+        match open(filename) {
+            Err(err) => eprintln!("{}: {}", filename, err),
+            Ok(_) => println!("Opened {}", filename)
+        }
+    }
     Ok(())
 }
 
@@ -67,7 +73,7 @@ pub fn get_args() -> MyResult<Config> {
     let mut bytes = matches.is_present("bytes");
     let chars = matches.is_present("chars");
 
-    if [lines, words, bytes, chars].iter().all(|v| *v == false) {
+    if [lines, words, bytes, chars].iter().all(|v| !v) {
         lines = true;
         words = true;
         bytes = true;
@@ -80,4 +86,11 @@ pub fn get_args() -> MyResult<Config> {
         bytes,
         chars
     })
+}
+
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?)))
+    }
 }
